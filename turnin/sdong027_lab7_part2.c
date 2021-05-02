aa2cb9c09d0693559b774364f235919c9ee86f8a
Pikasannnnn <dong.stephen11@gmail.com>
2021-05-01 22:30:56 -0700

    Completed most of all parts

---
+++ turnin/sdong027_lab7_part2.c
@@ -0,0 +1,38 @@
+/*	Author: sdong027
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #7  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *	Demo Link:
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned short photo_MIN = 0x030;	// 00 0011 0000
+unsigned short photo_MAX = 0x1E0;	// 01 1110 0000
+
+void ADC_init() {
+	ADCSRA |= ((1 << ADEN) | (1 << ADSC) | ( 1 << ADATE));
+}
+
+int main(void) {
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	//unsigned short input = 0x00;
+	ADC_init();
+	/* Insert your solution below */
+	while (1) {
+		unsigned short input = ADC;
+		PORTB = (char)input;
+		char upper = (input >> 8) & 0x03;
+		PORTD = upper;
+	}
+	return 1;
+}
